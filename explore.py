fd03049eea71f21555cb7fa0b8a50a4005487982
Albert Marsik <albert.marsik@gmail.com>
2023-05-26 20:08:25 +0200

    install wizard

---
+++ explore.py
@@ -0,0 +1,62 @@
+import fire
+import json
+from pyvis.network import Network
+import utils
+import os
+
+
+def make_graph(selection, file, data, hub):
+    net = Network(notebook=True, cdn_resources='in_line')
+    utils.add_nodes(net, selection, data)
+    net.repulsion(node_distance=150, spring_length=200)
+    hub_or_cue = lambda b: "hub" if b else "cue"
+    html = net.generate_html()
+    dest = f'graphs/{file}_{hub_or_cue(hub)}.html'
+    print(dest)
+    with open(dest, mode='w', encoding='utf-8') as fp:
+            fp.write(html)
+
+
+def filename(new_name=""):
+    "shows and edits name of destination file"
+    with open("config.json", "r+") as j:
+        config = json.load(j)
+        config["filename"] = new_name or config["filename"]
+        j.seek(0)
+        j.truncate(0)
+        json.dump(config, j)
+        print("\nTo change the destination filename, follow up command `name` with the desired name.")
+        print("Current filenames:")
+        return config['filename']
+
+
+def new_graph(*selection):
+    """makes a new graph, try python explore.py make play"""
+    if len(selection) == 0:
+        return """__________________________________________________________________
+Function 'make' creates a network graph in the 'graphs' directory:
+
+        Follow the command up by words you want
+        to be a part of the graph.
+        They just need to be seperated by space.
+
+        To change the name of desination file,
+        run the 'name' function
+__________________________________________________________________
+"""
+    selection = utils.make_selection(selection)
+    if not os.path.exists("graphs"):
+        os.makedirs("graphs")
+    file = filename()
+    data = utils.unpack_data()
+    colors = utils.hub_colors(selection)
+    make_graph(utils.make_nodes_h(selection, data, colors), file, data, hub=True)
+    make_graph(utils.make_nodes_c(selection, data, colors, hub=False), file, data, hub=False)
+    utils.combine_html_files(f"{file}_hub.html",f"{file}_cue.html", f"{file}_both.html")
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        "make": new_graph,
+        "name": filename,
+    })
---
+++ wizard.py
@@ -0,0 +1,24 @@
+import subprocess
+import platform
+import json
+import sys
+
+def stop_auto_setup(err, gen_err):
+    print("***",err,"***")
+    print("***",gen_err,"***")
+    sys.exit()
+
+with open("setup/setup_steps.json", "r") as i:
+    installation = json.load(i)
+
+# TODO - get type of platform and add platform specific commands in setup_steps.json
+if platform.system() != 'Darwin' and platform.system() != 'Linux':
+    stop_auto_setup("this autosetup only works on mac os and linux", installation["error"])
+
+# installation steps loop based on setup/setup_steps.json
+for step in installation["steps"]:
+    command = step["command"].get(platform, step["command"])
+    if subprocess.run(command).returncode != 0:
+        stop_auto_setup(step["error"], installation["error"])
+
+print(installation["success"])
