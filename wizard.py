c78d2745511e1d0e66ece8e4d6e91eac45dbbd73
Albert Marsik <albert.marsik@gmail.com>
2023-05-28 21:08:44 +0200

    windows wizard ver0

--- wizard.py
+++ wizard.py
@@ -7,14 +7,17 @@ import os
 def stop_auto_setup(err, gen_err):
     print("***",err,"***")
     print("***",gen_err,"***")
+    subprocess.run(["rm", "-rf", "venv"])
     sys.exit()
 
-with open("setup/setup_steps.json", "r") as i:
-    installation = json.load(i)
+inst_type = ""
 
-# TODO - get type of platform and add platform specific commands in setup_steps.json
+if platform.system() == 'Windows':
+    inst_type = "_w"
 if platform.system() != 'Darwin' and platform.system() != 'Linux':
-    stop_auto_setup("this autosetup only works on mac os and linux", installation["error"])
+    stop_auto_setup("this autosetup only works on mac os and linux and windows", "consult documentation for manual installation")
+with open(f"setup/setup_steps{inst_type}.json", "r") as i:
+    installation = json.load(i)
 
 # installation steps loop based on setup/setup_steps.json
 for step in installation["steps"]:
